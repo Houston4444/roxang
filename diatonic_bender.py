e46170b3627f30ddd960f033245e63f91aacc93a
houston <picotmathieu@gmail.com>
2019-06-20 15:38:00 +0200

    make repeatables intervals

--- diatonic_bender.py
+++ diatonic_bender.py
@@ -2,8 +2,70 @@
 
 import sys
 
-note_refs = ("C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B")
+READING_OFF = 0
+READING_COMMON_PROPERTIES = 1
+READING_INTERVAL_PROPERTIES = 2
+READING_SAMPLES = 3
 
+note_refs = ("C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B")
+parameters = {"KEY_TONE": "A", 
+              "SAMPLES_FOLDER": "", 
+              "KEY_INTERVAL": "c0-b9",
+              "PITCH_KEYCENTER": "c3",
+              "SAMPLES": "",
+              "LEVEL_SPLITS": "",
+              "INTERVAL_PROPERTIES": "",
+              "SFZ_FILE": "",
+              "COMMON_PROPERTIES": ""}
+
+class InputFileRange():
+    def __init__(self, lokey, hikey):
+        self.lokey = lokey
+        self.hikey = hikey
+        self.samples = []
+        self.pitch_keycenter = NoteKey("C", 3)
+        self.level_splits = []
+        self.sfz_properties = ""
+    
+    def setPitchKeycenter(self, note_key):
+        self.pitch_keycenter = note_key
+        
+    def addSample(self, sample):
+        self.samples.append(sample)
+    
+    def stringInterval(self):
+        return "%s-%s" % (self.lokey.toString(), self.hikey.toString())
+        
+    def setLevelSplits(self, string):
+        self.level_splits.clear()
+        
+        all_splits = string.split(',')
+        for split in all_splits:
+            try:
+                int_split = int(split.replace(' ', ''))
+                
+                if not 0 <= int_split <= 127:
+                    break
+                
+                self.level_splits.append(int_split)
+            except:
+                break
+        
+        print(self.level_splits)
+        
+        if len(self.level_splits) +1 != len(self.samples):
+            sys.stderr.write("Number of LEVEL_SPLITS not coherent with number of samples\n" +
+                             "will make automatic linear splits\n")
+            
+            self.level_splits.clear()
+            split_n = len(samples) -1
+            for i in range(split_n):
+                self.level_splits.append(int(128 * (i+1) / (split_n+1) -0.5))
+                
+    def addSfzProperty(self, string):
+        self.sfz_properties += string
+        self.sfz_properties += '\n'
+            
 class NoteKey():
     def __init__(self, note, octave):
         self.note = note.upper()
@@ -100,9 +162,6 @@ except:
     sys.stderr.write("%s is not a valid file" % input_file_name)
     sys.exit(1)
 
-
-key_tone = "A"
-key_interval = (NoteKey("C", 0), NoteKey("G#", 9))
 all_bends = {
     "C" : (-1, 2),
     "C#": (-3, 1),
@@ -116,48 +175,72 @@ all_bends = {
     "A" : (-2, 2),
     "A#": (-1, 2),
     "B" : (-2, 1)}
-samples_folder = ""
-samples = []
-sfz_file = ""
-in_all_groups = ""
 
-reading_in_all_groups = False
+in_all_groups = ""
+input_file_ranges = []
+
+reading = READING_OFF
 
 for line in input_contents.split('\n'):
-    if line == "_____in_all_groups______:":
-        reading_in_all_groups = True
+    if not line:
+        reading = READING_OFF
         continue
+    
+    if reading == READING_COMMON_PROPERTIES:
+        in_all_groups += line
+        in_all_groups += "\n"
         
-    if reading_in_all_groups:
-        if line == "________________________.":
-            reading_in_all_groups = False
-            continue
-        else:
-            in_all_groups += line
-            in_all_groups += "\n"
-    else:
-        if not line:
-            continue
+    elif reading == READING_INTERVAL_PROPERTIES:
+        if input_file_ranges:
+            input_file_ranges[-1].addSfzProperty(line)
         
+    elif reading == READING_SAMPLES:
         if line.startswith('#'):
             continue
         
-        if line.startswith('key_tone:'):
-            key_tone = line.partition('key_tone:')[2]
-        elif line.startswith('key_interval:'):
-            key_itv = line.partition('key_interval:')[2].split('-')
-            kn_lo = makeNoteKey(key_itv[0])
-            kn_hi = makeNoteKey(key_itv[1])
+        if input_file_ranges:
+            input_file_ranges[-1].addSample(line)
             
-            if kn_lo and kn_hi:
-                key_interval = (kn_lo, kn_hi)
+    elif reading == READING_OFF:
+        if line.startswith('#'):
+            continue
+        
+        for param in parameters:
+            if line.startswith(param + ':'):
+                value = line.partition(param + ':')[2]
+                
+                if param == "KEY_INTERVAL":
+                    key_itv = value.split('-')
+                    kn_lo = makeNoteKey(key_itv[0])
+                    kn_hi = makeNoteKey(key_itv[1])
             
-        elif line.startswith('samples_folder:'):
-            samples_folder = line.partition('samples_folder:')[2]
-        elif line.startswith('sample:'):
-            samples.append(line.partition('sample:')[2])
-        elif line.startswith('sfz_file:'):
-            sfz_file = line.partition('sfz_file:')[2]
+                    if kn_lo and kn_hi:
+                        input_file_ranges.append(InputFileRange(kn_lo, kn_hi))
+                    else:
+                        sys.stderr.write("wrong KEY_INTERVAL, abort !\n")
+                        sys.exit(1)
+                        
+                elif param == "PITCH_KEYCENTER":
+                    if input_file_ranges:
+                        input_file_ranges[-1].setPitchKeycenter(makeNoteKey(value))
+                        
+                elif param == "SAMPLES":
+                    reading = READING_SAMPLES
+                
+                elif param == "LEVEL_SPLITS":
+                    if input_file_ranges:
+                        input_file_ranges[-1].setLevelSplits(value)
+                        
+                elif param == "INTERVAL_PROPERTIES":
+                    reading = READING_INTERVAL_PROPERTIES
+                
+                elif param == "COMMON_PROPERTIES":
+                    reading = READING_COMMON_PROPERTIES
+                    
+                else:
+                    parameters[param] = value
+                    
+                break
         else:
             for note in note_refs:
                 if line.partition(':')[0].replace(' ', '') == note:
@@ -168,19 +251,10 @@ for line in input_contents.split('\n'):
                         bdown = int(bends_str[0])
                         bup   = int(bends_str[1])
                     except:
-                        continue
+                        sys.stderr.write("invalid bends for note %s, will take default values" % note)
                     
                     all_bends[note] = (bdown, bup)
                     break
-
-for note_key in key_interval:
-    print(note_key.note, note_key.octave)
-
-level_splits = []
-split_n = len(samples) -1
-for i in range(split_n):
-    level_splits.append(int(128 * (i+1) / (split_n+1) -0.5))
-
 
 #group bend notes
 grouped_bends = []
@@ -196,41 +270,51 @@ for bend in all_bends:
 for n in range(12):
     contents = ""
     
-    for bend_group in grouped_bends:
-        contents += "\n\n### bends at %s,%s ###############################\n" % (bend_group[0][0], bend_group[0][1])
+    for file_range in input_file_ranges:
+        contents += "\n\n\n### interval %s ________________________________________" \
+                        % file_range.stringInterval()
         
-        for i in range(len(level_splits)+1):
-            contents += "\n<group>\n"
-            contents += in_all_groups
-            if not in_all_groups.endswith('\n'):
-                contents += "\n"
-            contents += "bend_down=%i\n" % (bend_group[0][0] * 100)
-            contents += "bend_up=%i\n"   % (bend_group[0][1] * 100)
+        print(len(file_range.samples), len(file_range.level_splits))
+        for bend_group in grouped_bends:
+            contents += "\n\n##### bends at %s,%s ______________________________\n" \
+                            % (bend_group[0][0], bend_group[0][1])
             
-            if samples_folder:
-                contents += "sample=%s\%s\n" % (samples_folder, samples[i])
-            else:
-                contents += "sample=%s\n" % samples[i]
+            for i in range(len(file_range.samples)):
+                contents += "\n<group>\n"
+                contents += in_all_groups
+                if not in_all_groups.endswith('\n'):
+                    contents += "\n"
                 
-            if i != 0:
-                contents += "lolevel=%i\n" % (level_splits[i-1]+1)
-            if i != len(level_splits):
-                contents += "hilevel=%i\n" % level_splits[i]
-            
-            #contents += "\n"
-            
-            for j in range(9):
-                for note in bend_group[1:]:
-                    note_key = NoteKey(note, j).add(n)
-                    if key_interval[0] <= note_key <= key_interval[1]:
-                        contents += "<region> lokey=%s hikey=%s\n" % (note_key.toString(), note_key.toString())
+                contents += file_range.sfz_properties
+                
+                contents += "pitch_keycenter=%s\n" % file_range.pitch_keycenter.toString()
+                
+                contents += "bend_down=%i\n" % (bend_group[0][0] * 100)
+                contents += "bend_up=%i\n"   % (bend_group[0][1] * 100)
+                
+                if parameters["SAMPLES_FOLDER"]:
+                    contents += "sample=%s\%s\n" % (parameters["SAMPLES_FOLDER"], file_range.samples[i])
+                else:
+                    contents += "sample=%s\n" % file_range.samples[i]
+                    
+                if i != 0:
+                    contents += "lolevel=%i\n" % file_range.level_splits[i-1]
+                if i != len(file_range.samples) - 1:
+                    contents += "hilevel=%i\n" % (file_range.level_splits[i] -1)
+                
+                for j in range(9):
+                    for note in bend_group[1:]:
+                        note_key = NoteKey(note, j).add(n)
+                        if file_range.lokey <= note_key <= file_range.hikey:
+                            contents += "<region> lokey=%s hikey=%s\n" % (note_key.toString(), note_key.toString())
     
-    if "${KEY_TONE}" in sfz_file:
-        file = open(sfz_file.replace("${KEY_TONE}", noteplus(key_tone, n)) + ".sfz", 'w')
+    if "${KEY_TONE}" in parameters["SFZ_FILE"]:
+        file = open(parameters["SFZ_FILE"].replace("${KEY_TONE}", 
+                                                   noteplus(parameters["key_tone"], n)), 'w')
         file.write(contents)
         file.close()
     else:
-        file = open(sfz_file + ".sfz", 'w')
+        file = open(parameters["SFZ_FILE"], 'w')
         file.write(contents)
         file.close()
         # No Key Range given, make only one file
